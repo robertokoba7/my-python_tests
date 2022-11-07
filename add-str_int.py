93e90927125655a4ed4a292c5535049ed1ff0195
Robert Okoba <104755875+robertokoba7@users.noreply.github.com>
2022-11-07 15:54:28 +0300

    Delete add-str_int.py

--- add-str_int.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-#Declaring the variables
-my_int = 32
-my_str = "72"
-#explicitiy converting string into integer
-#before computing the sum
-my_sum = my_int + int(my_str)
-print (my_sum)  
